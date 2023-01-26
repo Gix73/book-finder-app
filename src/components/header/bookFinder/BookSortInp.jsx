29de9a6ecc9c0e7f6df70972ea2e9036cdbce0aa
Gix73 <gix73@yandex.ru>
2023-01-26 18:59:41 +0300

    feat: add options to select

--- src/components/header/bookFinder/BookSortInp.jsx
+++ src/components/header/bookFinder/BookSortInp.jsx
@@ -5,11 +5,22 @@ const BookSortInp = () => {
     <div className={s.sort_wrapper}>
       <div className={s.data_container}>
         <span>Categories</span>
-        <select className={s.select} name="" id=""></select>
+        <select className={s.select} name="" id="">
+          <option value="all">all</option>
+          <option value="art">art</option>
+          <option value="biography">biography</option>
+          <option value="computers">computers</option>
+          <option value="history">history</option>
+          <option value="medical">medical</option>
+          <option value="poetry">poetry</option>
+        </select>
       </div>
       <div className={s.data_container}>
         <span>Sorting by</span>
-        <select className={s.select} name="" id=""></select>
+        <select className={s.select} name="" id="">
+          <option value="relevance">relevance</option>
+          <option value="newest">newest</option>
+        </select>
       </div>
     </div>
   );
