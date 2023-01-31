bd361b412dbe5e5a2720d91b79bf2eb26a7fb42d
Gix73 <gix73@yandex.ru>
2023-01-31 16:07:01 +0300

    fix: crash, when no data in response

--- src/components/main/booksData/BookCard.jsx
+++ src/components/main/booksData/BookCard.jsx
@@ -5,15 +5,31 @@ const BookCard = (props) => {
   return (
     <>
       {props.bookItems.map((e) => {
-        let imgSrc = e.volumeInfo.imageLinks.thumbnail;
-        let authors = e.volumeInfo.authors.join(", ");
-        let title = e.volumeInfo.title;
-        let categories = e.volumeInfo.categories.join(" ");
+        let imgSrc = "";
+        let authors = "";
+        let title = "";
+        let categories = "";
+        let imgClass = s.hide;
+
+        if (e.volumeInfo.imageLinks && e.volumeInfo.imageLinks.thumbnail) {
+          imgSrc = e.volumeInfo.imageLinks.thumbnail;
+          imgClass = s.img;
+        }
+        if (e.volumeInfo.authors) {
+          authors = e.volumeInfo.authors.join(", ");
+        }
+        if (e.volumeInfo.title) {
+          title = e.volumeInfo.title;
+        }
+        if (e.volumeInfo.categories) {
+          categories = e.volumeInfo.categories[0];
+        }
+
         return (
           <Link to="/bookinfo" className={s.hover_link}>
             <div className={s.content_wrapper}>
               <div className={s.img_wrapper}>
-                <img src={imgSrc} className={s.img} alt="" />
+                <img src={imgSrc} className={imgClass} alt="book" />
               </div>
               <div className={s.text_wrapper}>
                 <span className={s.text}>{categories}</span>
@@ -29,3 +45,45 @@ const BookCard = (props) => {
 };
 
 export default BookCard;
+
+// const BookCard = (props) => {
+//   return (
+//     <>
+//       {props.bookItems.map((e) => {
+//         try {
+//           let imgSrc =
+//             typeof e.volumeInfo.imageLinks.thumbnail !== "undefined"
+//               ? e.volumeInfo.imageLinks.thumbnail
+//               : "";
+//           let authors =
+//             typeof e.volumeInfo.authors !== "undefined"
+//               ? e.volumeInfo.authors.join(", ")
+//               : "";
+//           let title =
+//             typeof e.volumeInfo.title !== "undefined" ? e.volumeInfo.title : "";
+//           let categories =
+//             typeof e.volumeInfo.categories[0] !== "undefined"
+//               ? e.volumeInfo.categories[0]
+//               : "";
+//           console.log(imgSrc + "," + authors + "," + title + "," + categories);
+//           return (
+//             <Link to="/bookinfo" className={s.hover_link}>
+//               <div className={s.content_wrapper}>
+//                 <div className={s.img_wrapper}>
+//                   <img src={imgSrc} className={s.img} alt="" />
+//                 </div>
+//                 <div className={s.text_wrapper}>
+//                   <span className={s.text}>{categories}</span>
+//                   <h3 className={s.text}>{title}</h3>
+//                   <span className={s.text}>{authors}</span>
+//                 </div>
+//               </div>
+//             </Link>
+//           );
+//         } catch (err) {
+//           console.log(err);
+//         }
+//       })}
+//     </>
+//   );
+// };
